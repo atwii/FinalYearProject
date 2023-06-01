393f1e2d69e583f4dc87cd459c684d8327a0503f
atwii <atwi00ali@gmail.com>
2023-06-02 01:32:45 +0300

    test

--- FYP/app/Http/Controllers/Controller.php
+++ FYP/app/Http/Controllers/Controller.php
@@ -9,4 +9,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    private function SalehAhbal(){
+
+
+        //testinng push
+    }
 }
